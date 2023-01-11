3c1bc913b64ca3f35366a328e87657322117d1f5
pavel <pavel.mizirev@gmail.com>
2023-01-11 15:21:51 +0300

    Update 17

--- src/main/java/Steps/css/cssElement/ElementDroppable.java
+++ src/main/java/Steps/css/cssElement/ElementDroppable.java
@@ -3,7 +3,6 @@ package Steps.css.cssElement;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;

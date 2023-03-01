6c27b5da0b9c64a7c46287d8927b4a409115e361
pavel <pavel.mizirev@gmail.com>
2023-03-01 12:05:53 +0300

    Update 82

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import tools.WebDrivers.WebDriver;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -13,7 +14,20 @@ import static com.codeborne.selenide.Selenide.open;
 public class BaseTest implements IAll{
     @BeforeAll
     static void beforeConfig() {
-        Configuration.browser= WebDriver.class.getName();
+        //Url удалённого веб драйвера
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        //Определяем какой браузер будем использовать
+        Configuration.browser = "chrome"/*WebDriver.class.getName()*/;
+        //Размер окна браузера
+        Configuration.browserSize = "1920x1080";
+        //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //Включить поддержку отображения экрана браузера во время выполнения теста
+        capabilities.setCapability("enableVNC", true);
+        //Включение записи видео в процессе выполнения тестов
+        capabilities.setCapability("enableVideo",  true);
+        //Переопределяем Browser capabilities
+        Configuration.browserCapabilities = capabilities;
         Configuration.timeout = 10000;
     }
     @BeforeEach
